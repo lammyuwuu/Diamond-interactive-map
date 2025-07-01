f296ebf74f04c2ddae53ff6a42bfcfb5d16f7776
lammyuwuu <k24116780@kcl.ac.uk>
2025-07-01 13:27:14 +0100

    add diamond map

--- map.js
+++ map.js
@@ -1,17 +1,28 @@
-let var1 = "hello"
-const var2 = "bye"
-let name = "Jason"
-let list=[1,2,3]
+// let var1 = "hello"
+// const var2 = "bye"
+// let name = "Jason"
+// let list=[1,2,3]
 
-function helloWorld(name) {
+// function helloWorld(name) {
     
-    {var word = "diamond"}
-    alert(word)
-}
+//     {var word = "diamond"}
+//     alert(word)
+// }
 
-function test() {
-    for(let val in list) {
-        console.log(i)
-    }
-    console.log(i)
-}
+// function test() {
+//     for(let val in list) {
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+
+// initialize the map on the "map" div with a given center and zoom
+var map = L.map('map', {
+    center: [51.574349, -1.310892],
+    zoom: 17.4
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
