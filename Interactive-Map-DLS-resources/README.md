a1ce14e0778866415d24da0541125b7ceb9f9861
lammyuwuu <k24116780@kcl.ac.uk>
2025-07-01 13:55:12 +0100

    over and under map + marker/circle

---
+++ Interactive-Map-DLS-resources/README.md
@@ -0,0 +1,3 @@
+# Diamond Light Source Work Experience
+
+Resources that are needed to build an interactive map of diamond.
--- map.js
+++ map.js
@@ -26,3 +26,19 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+var baseOver = 'img/BaseOver.png'
+var baseUnder = 'img/BaseUnder.png'
+var imageBounds = [[51.57168183170403, -1.3173294067382815], [51.57701619673675, -1.304454803466797]];
+L.imageOverlay(baseOver, imageBounds, {opacity:1, zIndex:3}).addTo(map);
+L.imageOverlay(baseUnder, imageBounds, {opacity:1, zIndex:2}).addTo(map);
+
+
+var marker = L.marker([51.574349, -1.310892]).addTo(map);
+
+var circle = L.circle([51.574349, -1.310892], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 50
+}).addTo(map);
