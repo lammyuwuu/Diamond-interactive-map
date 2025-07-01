b36da9f86b0a4ecafa769306f154d89e352544ac
lammyuwuu <k24116780@kcl.ac.uk>
2025-07-01 11:28:54 +0100

    adding variables/ buttons

---
+++ map.js
@@ -0,0 +1,17 @@
+let var1 = "hello"
+const var2 = "bye"
+let name = "Jason"
+let list=[1,2,3]
+
+function helloWorld(name) {
+    
+    {var word = "diamond"}
+    alert(word)
+}
+
+function test() {
+    for(let val in list) {
+        console.log(i)
+    }
+    console.log(i)
+}
