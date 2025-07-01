aeff4acd06a84e331e0fd5ba247c6b10f79e1639
lammyuwuu <k24116780@kcl.ac.uk>
2025-07-01 14:58:18 +0100

    beamline markers

--- map.js
+++ map.js
@@ -34,11 +34,41 @@ L.imageOverlay(baseOver, imageBounds, {opacity:1, zIndex:3}).addTo(map);
 L.imageOverlay(baseUnder, imageBounds, {opacity:1, zIndex:2}).addTo(map);
 
 
-var marker = L.marker([51.574349, -1.310892]).addTo(map);
 
-var circle = L.circle([51.574349, -1.310892], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 50
-}).addTo(map);
+// // Adding marker/ circle into map
+
+// var marker = L.marker([51.574349, -1.310892]).addTo(map);
+
+// var circle = L.circle([51.574349, -1.310892], {
+//     color: 'red',
+//     fillColor: '#f03',
+//     fillOpacity: 0.5,
+//     radius: 50
+// }).addTo(map);
+
+
+// fetching beamlines coordinates from Diamond provided json file
+// then add marker for each beamline
+
+fetch('resources/beamlines_data.json')
+.then((response) => response.json())
+.then((bGroups) => {
+    console.log(bGroups)
+    beamlines=[]
+    for (const group of bGroups) {
+        for (const beam of group["beamlines"]){
+            coordinates=beam["position"]
+            //beamlines.push(coordinates)
+            var marker = L.marker(coordinates).addTo(map);
+            // var circle = L.circle(coordinates, {
+            //     color: 'red',
+            //     fillColor: '#f03',
+            //     fillOpacity: 0.5,
+            //     radius: 25
+            // }).addTo(map);
+        }
+    }
+    console.log(beamlines)
+})
+
+
